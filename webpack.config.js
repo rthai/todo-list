11b6c2ca1e6df9ebc29d25cfef62797ae830b024
rthai <ahnamiko@gmail.com>
2018-07-09 17:16:21 -0700

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: [
+          'react-hot',
+          'babel'
+        ]
+      }
+    ]
+  }
+};
