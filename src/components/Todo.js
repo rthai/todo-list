9731aeb55c8a4cc3adf6bb313c503f0f23457500
rthai <ahnamiko@gmail.com>
2018-07-17 22:32:16 -0700

    change to stateful component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Todo = (props) => (
-  <li>{props.todo.todo}</li>
-)
+// when pencil is clicked, change li class name to editing
+export default class Todo extends Component {
+  state = {
+    editing: false,
+  }
 
-export default Todo;
+
+
+  render() {
+    return (
+      <li className="">
+      <input type="checkbox"/>
+      <label>{this.props.todo.todo}</label>
+      <a className="update-todo"
+        /** onClick={() => this.props.updateTodo(this.props.index, todo)}**/>
+        ✎
+      </a>
+      <a 
+        className="remove-todo"
+        onClick={() => this.props.removeTodo(this.props.index)}>
+        ✕
+      </a> 
+      {/* <input className="edit"/> */}
+    </li>
+    )
+  }
+}
+
+
+// const Todo = (props) => (
+//   <li className="">
+//     <input type="checkbox"/>
+//     <label>{props.todo.todo}</label>
+//     <a className="update-todo"
+//       onClick={() => props.updateTodo(props.index, todo)}>
+//       ✎
+//     </a>
+//     <a 
+//       className="remove-todo"
+//       onClick={() => props.removeTodo(props.index)}>
+//       x
+//     </a> 
+//     <input className="edit"/>
+//   </li>
+// )
+
+// export default Todo;
